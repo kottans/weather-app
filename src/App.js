fd8fe8ae6e0feaa674d4deea265bd4c5f1c96e29
Oleksiy Rudenko <oleksiy.rudenko@gmail.com>
2018-12-07 15:00:33 +0200

    app: Add Temperature and nest it into App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import Temperature from "./Temperature.js";
+
 export default class App {
   /**
    * Instantiates App component
    * @param {HTMLElement} host
    */
   constructor(host) {
-    host.innerHTML = 'Weather';
+    // Since component requires host as an HTMLElement we need to change rendering approach
+    // 1. Create text block
+    // 1a. Create container for text
+    const titleContainer = document.createElement('div');
+    // 1b. Assign text
+    titleContainer.innerHTML = 'Weather';
+    // 2. Create temperature block
+    // 2a. Create container for Temperature component
+    const temperatureContainer = document.createElement('div');
+    // 2b. Instantiate Temperature component to render into container
+    new Temperature(temperatureContainer);
+    // 3. Attach containers to the App's host
+    [titleContainer, temperatureContainer].forEach(element => host.appendChild(element));
   }
 }
---
+++ src/Temperature.js
@@ -0,0 +1,9 @@
+export default class Temperature {
+  /**
+   * Instantiates Temperature component
+   * @param {HTMLElement} host
+   */
+  constructor(host) {
+    host.innerHTML = "37&deg;C";
+  }
+}
